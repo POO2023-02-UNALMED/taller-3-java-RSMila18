e07d158777c53de5503ff2d94b07a5c92e56945c
Camila Rios <mriosm@unal.edu.co>
2023-08-28 20:51:40 -0500

    123

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -4,17 +4,14 @@ public class Marca {
 	private String nombre;
 	
 	public Marca(String nombre){
-
 		this.nombre = nombre;
 	}
 
 	public String getNombre() {
-
 		return nombre;
 	}
 
 	public void setNombre(String nombre) {
-
 		this.nombre = nombre;
 	}
 }
